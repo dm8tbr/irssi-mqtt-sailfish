80ee10694fafcad593e091dddf6b3c5add4a2791
Thomas B. Ruecker <thomas@ruecker.fi>
2014-08-01 15:59:46 +0300

    Subscribe on reconnect, set will for disconnect

--- irssi-notification-client.py
+++ irssi-notification-client.py
@@ -17,6 +17,13 @@ def read_credentials_file(filename):
 def on_log(mosq, obj, level, string):
     print(string)
 
+def on_connect(mosq, userdata, rc):
+    if rc == 0:
+        print("Connected to: "+mqtt_server)
+        mqtt.subscribe("sailfish/tbr/irssi/notifications", 2)
+        mqtt.publish("sailfish/tbr/irssi/receiver_state", "connected", 0, True)
+    else:
+        print("Connection failed with error code: "+rc)
 
 mqtt_name = "sailfish_iot_"+''.join(random.choice(string.ascii_lowercase + string.digits) for i in xrange(8))
 mqtt_server = "mqtt"
@@ -28,9 +35,10 @@ mqtt = mosquitto.Mosquitto(mqtt_name)
 
 mqtt.username_pw_set(mqtt_user, mqtt_password)
 mqtt.on_log = on_log
-mqtt.reconnect_delay_set(1, 300, 'true')
+mqtt.on_connect = on_connect
+mqtt.will_set("sailfish/tbr/irssi/receiver_state", None, 0, True)
+mqtt.reconnect_delay_set(1, 300, True)
 mqtt.connect(mqtt_server, mqtt_port, mqtt_keepalive)
-mqtt.subscribe("sailfish/tbr/irssi", 0)
 
 def on_message(mosq, obj, msg):
     print("Message received on topic "+msg.topic+" with QoS "+str(msg.qos)+" and payload "+msg.payload)
